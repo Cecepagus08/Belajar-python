2e2a473520871702c96064771ebf6e1449a2a9e4
Cecepagus06 <cecepagus406@gmail.com>
2024-07-27 03:58:53 +0700

    27-7-2024

---
+++ rumusTrapesium.py
@@ -0,0 +1,11 @@
+print("====================================")
+print("RUMUS HITUNG TRAPESIUM DENGAN PYTHON");
+print("====================================")
+
+alasAtas = float(input("masukkan alas atas : "))
+alasBawah = float(input("masukkan alas bawah : "))
+tinggi = float(input("masukkan tinggi : "))
+
+hasil =  0.5 * (alasAtas + alasBawah) * tinggi;
+
+print(f"{hasil}")
